668fe4d86703e9186baf238599c2c55d68c0dc46
majestickewldud <96453808+qayyumayaan@users.noreply.github.com>
2023-04-27 15:29:06 -0400

    Functional Game with Legal Moves

--- js/chessing.js
+++ js/chessing.js
@@ -1,17 +1,89 @@
-var pos = 'start'
-// var pos = 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R'
-var config = {
-    draggable: true,
-    position: pos,
-    dropOffBoard: 'trash',
-    onDrop: function(source, target, piece, newPos, oldPos, orientation) {
-        console.log('Piece ' + piece + ' dropped on ' + target);
-    }
+// imports chess.js via cloudflare
+
+var board = null
+var game = new Chess()
+var $status = $('#status')
+var $fen = $('#fen')
+var $pgn = $('#pgn')
+
+function onDragStart (source, piece, position, orientation) {
+  // do not pick up pieces if the game is over
+  if (game.game_over()) return false
+
+  // only pick up pieces for the side to move
+  if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+    return false
+  }
 }
-var board = Chessboard('board', config);
+
+function onDrop (source, target) {
+  // see if the move is legal
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+  })
+
+  // illegal move
+  if (move === null) return 'snapback'
+
+  updateStatus()
+}
+
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+function onSnapEnd () {
+  board.position(game.fen())
+}
+
+function updateStatus () {
+  var status = ''
+
+  var moveColor = 'White'
+  if (game.turn() === 'b') {
+    moveColor = 'Black'
+  }
+
+  // checkmate?
+  if (game.in_checkmate()) {
+    status = 'Game over, ' + moveColor + ' is in checkmate.'
+  }
+
+  // draw?
+  else if (game.in_draw()) {
+    status = 'Game over, drawn position'
+  }
+
+  // game still on
+  else {
+    status = moveColor + ' to move'
+
+    // check?
+    if (game.in_check()) {
+      status += ', ' + moveColor + ' is in check'
+    }
+  }
+
+  $status.html(status)
+  $fen.html(game.fen())
+  $pgn.html(game.pgn())
+}
+
+
+var pos = 'start'
+// // var pos = 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R'
+
+var config = {
+  draggable: true,
+  position: pos,
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd
+}
+board = Chessboard('board', config)
+
+updateStatus()
 
 $('#startBtn').on('click', board.start);
 $('#clearBtn').on('click', board.clear);
-$('#move2Btn').on('click', function () {
-    board.move('d2-d4', 'g8-f6')
-})
